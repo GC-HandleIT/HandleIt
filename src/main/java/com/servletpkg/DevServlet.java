578c85d72d2e4d68d7b8881b82efa2c52e61d21c
Erin Bergman <erin.e.bergman@gmail.com>
2016-05-10 17:44:35 -0400

    Finished fixing all these bugs

--- src/main/java/com/servletpkg/DevServlet.java
+++ src/main/java/com/servletpkg/DevServlet.java
@@ -7,9 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.HandleIT.DevObj;
+
 @WebServlet("/DevServlet")
 public class DevServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	DevObj devToAddToDB = new DevObj();
+	DevObj searchToLogin = new DevObj();
 
 	public DevServlet() {
 		super();
@@ -22,7 +27,31 @@ public class DevServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doGet(request, response);
-	}
 
+		String theID = request.getParameter("form-id");
+
+		if (!(theID.equals(null))) {
+
+			devToAddToDB.setId(request.getParameter("form-id"));
+			devToAddToDB.setFirstName(request.getParameter("form-first_name"));
+			devToAddToDB.setLastName(request.getParameter("form-last_name"));
+			devToAddToDB.setPassword(request.getParameter("form-password"));
+			devToAddToDB.setEmailAddress(request.getParameter("form-email_address"));
+			devToAddToDB.setLocation(request.getParameter("form-location"));
+			devToAddToDB.setPicUrl(request.getParameter("form-picture_url"));
+			devToAddToDB.setProfileUrl(request.getParameter("form-profile_url"));
+			devToAddToDB.setProjects(request.getParameter("form-projects"));
+			devToAddToDB.setSkills(request.getParameter("form-skills"));
+
+			System.out.println("Collected a dev table entry.");
+
+			DevConn.writeToDevTable(devToAddToDB);
+
+			if (DevConn.passOrFail) {
+				response.sendRedirect("writedevtable.html");
+				System.out.println("Dev has been added to the db.");
+			}
+		} // else
+
+	}
 }
